212e61591d7bc3325d3b5f39df01dfd21a358d5b
Павел <fotocrysis36@gmail.com>
2022-01-17 16:17:14 +0300

    changes

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, request
-from functions import open_json, count_comments, get_comments_by_pk, replace_hashtags_with_links, search_post_by_username, search_post_by_key
-app=Flask("__name__")
+from functions import open_json, count_comments, get_comments_by_pk, replace_hashtags_with_links, \
+    search_post_by_username, search_post_by_key
+
+app = Flask("__name__")
+
 
 @app.route("/")
 def main_page():
-    posts=count_comments("data/comments.json")
+    posts = count_comments("data/comments.json")
     return render_template("index.html", posts=posts, number_coms=count_comments("data/comments.json"))
+
+
 @app.route("/post/<pk>")
 def post_page(pk):
-    post=open_json("data/data.json")
+    post = open_json("data/data.json")
     comments_count = len(get_comments_by_pk(pk))
-    post_comments=get_comments_by_pk(pk)
-    post[int(pk)-1]["content"]=replace_hashtags_with_links(post[int(pk)-1]["content"])
-    return render_template("post.html", post=post[int(pk)-1], post_comments=post_comments, comments_count=comments_count)
+    post_comments = get_comments_by_pk(pk)
+    post[int(pk) - 1]["content"] = replace_hashtags_with_links(post[int(pk) - 1]["content"])
+    return render_template("post.html", post=post[int(pk) - 1], post_comments=post_comments,
+                           comments_count=comments_count)
+
 
 @app.route("/user/<username>")
 def user_page(username):
-    user_posts=search_post_by_username(username)
+    user_posts = search_post_by_username(username)
     return render_template("user-feed.html", user_posts=user_posts)
+
 
 @app.route("/search")
 def search_page():
-    key=request.args.get("s")
-    posts=search_post_by_key(key)
-    posts_len=len(posts)
+    key = request.args.get("s")
+    posts = search_post_by_key(key)
+    posts_len = len(posts)
     return render_template("search.html", posts=posts, posts_len=posts_len)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run()
